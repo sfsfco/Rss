19c27cd3d5d5250e6bcc6b3c970d2630902a9933
Mostafa Khattab <sfsfco@gmail.com>
2022-06-17 23:52:49 +0100

    Create database if not exist

--- src/database.js
+++ src/database.js
@@ -1,6 +1,24 @@
 var Sequelize = require('sequelize');
+const mysql = require("mysql2");
 var config = require('./config/config');
 
+//await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
+const mysqlConnection = mysql.createConnection({
+  host: config.host,
+  user: config.user,
+  password: config.password,
+});
+
+mysqlConnection.query(
+  `CREATE DATABASE IF NOT EXISTS ${config.database}`,
+  function (err, results) {
+    console.log('Connected to MySQL database');
+    
+  }
+);
+
+// Close the connection
+mysqlConnection.end();
 
 const database = new Sequelize(
   config.database, // name database
